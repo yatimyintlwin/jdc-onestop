e4617abb4cffdf2ec77bbf0479a34f416d977c63
yatimyintlwin <yatimyintlwin43@gmail.com>
2024-06-20 16:05:20 +0630

    [Added] About Method

---
+++ language-basic/variables/MethodDemo.java
@@ -0,0 +1,22 @@
+public class MethodDemo {
+
+	void sayHello() {
+		System.out.println("Hello");
+	}
+
+	int add(int a, int b) {
+
+		return a + b;
+	}
+
+	void print(int times) {
+
+		if(times < 0) {
+			return;
+		}
+
+		for(int i = 0; i < times; i ++) {
+			System.out.println("Hello");
+		}
+	}
+}
--- language-basic/variables/OwnerDemo.java
+++ language-basic/variables/OwnerDemo.java
@@ -1,3 +1,35 @@
 public class OwnerDemo {
-	
+
+	static String message = "Static Message";
+	static int count;
+
+	String name = "Instance Name";
+	int agee;
+
+	void hidingDemo() {
+		String name = "Local Name";
+		System.out.println(name);
+		System.out.println(this.name);
+
+		String message = "Local Message";
+		System.out.println(message);
+		System.out.println(OwnerDemo.message);
+	}
+
+	void doSomething(int count) {
+
+		String data;
+
+		data = "Hello";
+
+		System.out.println(data);
+
+		System.out.println(count);
+
+		for(int i; i < count; i ++) {
+
+		}
+
+		System.out.println(i);
+	}
 }
