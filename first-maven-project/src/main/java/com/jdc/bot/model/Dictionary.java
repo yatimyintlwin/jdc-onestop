29d42ab35a6d7e81f45c7c75d73b81d56fa09441
yatimyintlwin <yatimyintlwin43@gmail.com>
2024-07-16 23:47:40 +0630

    [Added] First Maven Project

--- first-maven-project/src/main/java/com/jdc/bot/model/Dictionary.java
+++ first-maven-project/src/main/java/com/jdc/bot/model/Dictionary.java
@@ -1,29 +1,84 @@
 package com.jdc.bot.model;
 
-import java.util.Arrays;
-
 public class Dictionary {
 
-	public String[] array = {};
+	private Entry[] entries;
+	private int capacity;
+	private int size;
 
-	public void register(String question, String answer) {
-		var index = question.hashCode();
+	public Dictionary(int capacity) {
+		super();
+		this.capacity = capacity;
+		this.entries = new Entry[capacity];
+	}
 
-		if (array.length < index) {
-			array = Arrays.copyOf(array, index + 1);
+	public int register(String question, String answer) {
+
+		if (size < capacity) {
+			var hash = hash(question);
+			var entry = entries[hash];
+
+			if (null == entry) {
+				entries[hash] = new Entry(question, answer);
+			} else {
+				if (entry.question().equals(question)) {
+					entries[hash] = new Entry(question, answer);
+				} else {
+					var index = (hash + 1) % capacity;
+					while (true) {
+						if (null == entries[index]) {
+							entries[index] = new Entry(question, answer);
+							break;
+						}
+
+						index = (index + 1) % capacity;
+					}
+				}
+			}
+
+			return ++size;
 		}
 
-		array[index] = answer;
+		return -1;
 	}
 
 	public String search(String question) {
 
-		var index = question.hashCode();
+		if (size > 0) {
+			var hash = hash(question);
+			var entry = entries[hash];
 
-		if (array.length > index) {
-			return array[index];
+			if (null != entry) {
+				if (entry.question().equals(question)) {
+					return entry.answer();
+				}
+
+				var index = (hash + 1) % capacity;
+
+				for (;;) {
+
+					if (hash == index) {
+						break;
+					}
+
+					var next = entries[index];
+					if (next.question().equals(question)) {
+						return next.answer();
+					}
+
+					index = (index + 1) % capacity;
+				}
+			}
 		}
 
 		return null;
+	}
+
+	private int hash(String question) {
+		var hash = question.hashCode() % capacity;
+		return hash < 0 ? (hash * -1) : hash;
+	}
+
+	private static record Entry(String question, String answer) {
 	}
 }
