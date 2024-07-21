db60e969b35647f6fda70f605c3047de74f83b65
yatimyintlwin <yatimyintlwin43@gmail.com>
2024-07-21 21:46:17 +0630

    [Added] OOP

---
+++ oop/oop-01-encapsulation/src/main/java/com/jdc/online/demo/StudentRepository.java
@@ -0,0 +1,20 @@
+package com.jdc.online.demo;
+
+public class StudentRepository {
+
+	public int add(Student input) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Student findByID(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Student[] getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
