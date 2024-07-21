da110890df46f9c634d1f810cbec045c70c799eb
yatimyintlwin <yatimyintlwin43@gmail.com>
2024-07-21 21:45:02 +0630

    [Added] First Maven Project

--- first-maven-project/src/test/java/com/jdc/bot/model/DictionaryTest.java
+++ first-maven-project/src/test/java/com/jdc/bot/model/DictionaryTest.java
@@ -17,11 +17,17 @@ public class DictionaryTest {
 
 	@Order(1)
 	@ParameterizedTest
-	@CsvSource(value = { "How are you\tI am fine, thank you.\t1", "Hi\tHello\t2",
-			"How old are you\tI am just a child.\t3", "Do you like movie\tYes I like it.\t4",
-			"Do you like ice cream\tThis is not my choice\t5", "Where do you live\tIn your heart.\t6",
-			"What is your name\tMr Bot.\t7", "Which color do you like\tI like blue.\t8", "Bye!\tBye Bye, thank you.\t9",
-			"Take care!\tThank you.\t10", }, delimiter = '\t')
+	@CsvSource(value = { "How are you\tI am fine, thank you.\t1", 
+			"Hi\tHello\t2",
+			"How old are you\tI am just a child.\t3", 
+			"Do you like movie\tYes I like it.\t4",
+			"Do you like ice cream\tThis is not my choice\t5", 
+			"Where do you live\tIn your heart.\t6",
+			"What is your name\tMr Bot.\t7", 
+			"Which color do you like\tI like blue.\t8", 
+			"Bye!\tBye Bye, thank you.\t9",
+			"Take care!\tThank you.\t10",
+			}, delimiter = '\t')
 	void test_register(String question, String answer, int size) {
 		int result = dict.register(question, answer);
 		assertEquals(size, result);
@@ -29,10 +35,17 @@ public class DictionaryTest {
 
 	@Order(2)
 	@ParameterizedTest
-	@CsvSource(value = { "How are you\tI am fine, thank you.", "Hi\tHello", "How old are you\tI am just a child.",
-			"Do you like movie\tYes I like it.", "Do you like ice cream\tThis is not my choice",
-			"Where do you live\tIn your heart.", "What is your name\tMr Bot.", "Which color do you like\tI like blue.",
-			"Bye!\tBye Bye, thank you.", "Take care!\tThank you.", }, delimiter = '\t')
+	@CsvSource(value = { "How are you\tI am fine, thank you.", 
+			"Hi\tHello", 
+			"How old are you\tI am just a child.",
+			"Do you like movie\tYes I like it.", 
+			"Do you like ice cream\tThis is not my choice",
+			"Where do you live\tIn your heart.", 
+			"What is your name\tMr Bot.", 
+			"Which color do you like\tI like blue.",
+			"Bye!\tBye Bye, thank you.", 
+			"Take care!\tThank you.", 
+			}, delimiter = '\t')
 	void test_search(String question, String answer) {
 
 		String result = dict.search(question);
