fd723f4fa4e29cac5cb0b55640667a2b0eab81fd
yatimyintlwin <yatimyintlwin43@gmail.com>
2024-08-04 19:12:57 +0630

    [Added] Hiding Members

--- oop/oop-02-inheritance/src/main/java/com/jdc/demo/hiding/ChildType.java
+++ oop/oop-02-inheritance/src/main/java/com/jdc/demo/hiding/ChildType.java
@@ -2,11 +2,28 @@ package com.jdc.demo.hiding;
 
 public class ChildType extends SuperType {
 
-	public void showFormChild() {
+	private String instanceVariable = "Variable From Child";
+	private static String staticVariable = "Static Variable From Child";
+
+	public static void doStaticMathod() {
+		System.out.println("Static Method From Child");
+	}
+
+	public void showFromChild() {
+
 		System.out.printf("Instance Variable : %s.%n", instanceVariable);
-		
+
 		System.out.printf("Static Variable : %s.%n", staticVariable);
-		
+
 		doStaticMathod();
+	}
+
+	public void showFromChildForHidingMember() {
+
+		System.out.printf("Instance Variable : %s.%n", super.instanceVariable);
+
+		System.out.printf("Static Variable : %s.%n", SuperType.staticVariable);
+
+		SuperType.doStaticMathod();
 	}
 }
--- oop/oop-02-inheritance/src/main/java/com/jdc/demo/hiding/SuperType.java
+++ oop/oop-02-inheritance/src/main/java/com/jdc/demo/hiding/SuperType.java
@@ -4,7 +4,7 @@ public class SuperType {
 
 	protected String instanceVariable = "Variable From Parent";
 	protected static String staticVariable = "Static Variable From Parent";
-	
+
 	public static void doStaticMathod() {
 		System.out.println("Static Method From Parent");
 	}
