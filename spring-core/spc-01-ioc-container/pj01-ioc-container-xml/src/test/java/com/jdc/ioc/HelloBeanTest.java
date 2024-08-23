a461c40f405efc28f88338821f0183b96312dd80
yatimyintlwin <yatimyintlwin43@gmail.com>
2024-08-23 16:41:33 +0630

    [Add] IoC Container

---
+++ spring-core/spc-01-ioc-container/pj01-ioc-container-xml/src/test/java/com/jdc/ioc/HelloBeanTest.java
@@ -0,0 +1,17 @@
+package com.jdc.ioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class HelloBeanTest {
+
+	@Test
+	void test() {
+		
+		try(var context = new GenericXmlApplicationContext("classpath:/application.xml")) {
+	
+			var bean = context.getBean(HelloBean.class);
+			System.out.println(bean.sayHello());
+		}
+	}
+}
---
+++ spring-core/spc-01-ioc-container/pj02-ioc-container-annotation/src/test/java/com/jdc/ioc/HelloBeanTest.java
@@ -0,0 +1,15 @@
+package com.jdc.ioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class HelloBeanTest {
+
+	@Test
+	void test() {
+		
+		try(var context = new AnnotationConfigApplicationContext()) {
+			
+		}
+	}
+}
