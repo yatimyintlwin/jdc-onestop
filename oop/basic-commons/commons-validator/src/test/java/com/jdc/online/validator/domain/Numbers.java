1e530e0d9b557d097f3fdff0fca6f670671fe0db
yatimyintlwin <yatimyintlwin43@gmail.com>
2024-09-24 23:47:34 +0630

    [Update] Validator

---
+++ oop/basic-commons/commons-validator/src/test/java/com/jdc/online/validator/domain/Numbers.java
@@ -0,0 +1,30 @@
+package com.jdc.online.validator.domain;
+
+import com.jdc.online.validator.annotations.MaxValue;
+import com.jdc.online.validator.annotations.MinValue;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Numbers {
+
+	@MinValue(value = 10)
+	@MaxValue(value = 100)
+	private byte byteValue;
+	
+	@MinValue(value = 10)
+	@MaxValue(value = 100)
+	private short shortValue;
+	
+	@MinValue(value = 10)
+	@MaxValue(value = 100)
+	private int intValue;
+	
+	@MinValue(value = 10)
+	@MaxValue(value = 100)
+	private long longValue;
+}
